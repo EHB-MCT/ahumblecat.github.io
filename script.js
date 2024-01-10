f71fd19d229004cf67c484ada904657e9a887d7f
ahumblecat <ayman.abid@student.ehb.be>
2024-01-10 16:14:48 +0100

    alles dat ik kon tot hier maken. mist nog veel toch well...

--- script.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-	console.log("toggleMenu function called");
-	const menu = document.querySelector(".menu-links");
-	const icon = document.querySelector(".hamburger-icon");
-	menu.classList.toggle("open");
-	icon.classList.toggle("open");
-}
